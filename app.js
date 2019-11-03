1bc64d8aeb2b0028421c46754e998aff3b878899
Антон Наумович <antonnaumovich1992@gmail.com>
2019-11-03 12:40:54 +0300

    commit2

--- app.js
+++ app.js
@@ -1,147 +1,142 @@
-const CONTROL_SIZE = 50;
+const SIZE_CONTROL = 50;
 
 let cropData = {
-    radius: 0,
-    x: 0,
-    y: 0
-  },
-  canvas = document.getElementById("imagePreview"),
-  cropCanvas = document.createElement("canvas"),
-  mode = null,
-  changeMade = false,
-  frameRequest;
+        radius: 0,
+        x: 0,
+        y: 0,
+    },
+    canvas = document.getElementById('imagePreview'),
+    cropCanvas = document.createElement('canvas'),
+    mode = null,
+    changeMade = false,
+    frameRequest;
 
-document.forms.fileChooser.elements.file.addEventListener("change", function() {
-  this.blur();
-  let file = this.files[0];
-  if (file) {
-    cancelAnimationFrame(frameRequest);
-    loadImage(file).then(image => {
-      cropData.radius = Math.min(image.width, image.height) / 6;
-      cropData.x = image.width / 2;
-      cropData.y = image.height / 2;
-      canvas.width = cropCanvas.width = image.width;
-      canvas.height = cropCanvas.height = image.height;
-      changeMade = true;
-      render(image);
-    });
-  }
+document.forms.fileChooser.elements.file.addEventListener('change', function () {
+    this.blur();
+    let file = this.files[0];
+    if (file) {
+        cancelAnimationFrame(frameRequest);
+        loadImage(file).then((image) => {
+            cropData.radius = Math.min(image.width, image.height) / 6;
+            cropData.x = image.width / 2;
+            cropData.y = image.height / 2;
+            canvas.width = cropCanvas.width = image.width;
+            canvas.height = cropCanvas.height = image.height;
+            changeMade = true;
+            render(image);
+
+        });
+    }
 });
 
-document.forms.saveForm.addEventListener("submit", event => {
-  event.preventDefault();
-  let fileName = this.elements.fileName.value + ".png";
+document.forms.saveForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    let fileName = this.elements.fileName.value + '.jpeg';
 
-  let saveCanvas = document.createElement("canvas"),
-    width = (saveCanvas.width = cropData.radius * 2),
-    height = (saveCanvas.height = cropData.radius * 2),
-    { x, y, radius } = cropData;
+    let saveCanvas = document.createElement('canvas'),
+        width = saveCanvas.width = cropData.radius * 2,
+        height = saveCanvas.height = cropData.radius * 2,
+        {
+            x,
+            y,
+            radius
+        } = cropData;
 
-  let ctx = saveCanvas.getContext("2d");
-  ctx.clearRect(0, 0, width, height);
-  ctx.drawImage(
-    canvas,
-    x - radius,
-    y - radius,
-    width,
-    height,
-    0,
-    0,
-    width,
-    height
-  );
-  ctx.globalCompositeOperation = "destination-in";
-  ctx.beginPath();
-  ctx.arc(radius, radius, radius, 0, 2 * Math.PI);
-  ctx.closePath();
-  ctx.fill();
+    let ctx = saveCanvas.getContext('2d');
+    ctx.clearRect(0, 0, width, height);
+    ctx.drawImage(canvas, x - radius, y - radius, width, height, 0, 0, width, height);
+    ctx.globalCompositeOperation = 'destination-in';
+    ctx.beginPath();
+    ctx.arc(radius, radius, radius, 0, 2 * Math.PI);
+    ctx.closePath();
+    ctx.fill();
 
-  saveCanvas.toBlob(function(blob) {
-    let a = document.createElement("a");
-    a.download = fileName;
-    a.href = URL.createObjectURL(blob);
-    a.dispatchEvent(new MouseEvent("click"));
-    URL.revokeObjectURL(a.href);
-  });
+    saveCanvas.toBlob(function (blob) {
+        let a = document.createElement('a');
+        a.download = fileName;
+        a.href = URL.createObjectURL(blob);
+        a.dispatchEvent(new MouseEvent('click'));
+        URL.revokeObjectURL(a.href);
+    })
 });
 
-canvas.addEventListener("mousedown", event => {
-  let x = event.offsetX,
-    y = event.offsetY;
+canvas.addEventListener('mousedown', (event) => {
+    let x = event.offsetX,
+        y = event.offsetY;
 
-  if (isResizeMode(x, y)) {
-    mode = "resize";
-  } else {
-    mode = "move";
-  }
+    if (isResizeMode(x, y)) {
+        mode = 'resize';
+    } else {
+        mode = 'move';
+    }
+
 });
 
-canvas.addEventListener("mousemove", event => {
-  if (mode === "resize") {
-    cropData.radius = Math.abs(event.offsetX - cropData.x);
-    changeMade = true;
-  } else if (mode === "move") {
-    cropData.x = event.offsetX;
-    cropData.y = event.offsetY;
-    changeMade = true;
-  }
+canvas.addEventListener('mousemove', (event) => {
+    if (mode === 'resize') {
+        cropData.radius = Math.abs(event.offsetX - cropData.x);
+        changeMade = true;
+    } else if (mode === 'move') {
+        cropData.x = event.offsetX;
+        cropData.y = event.offsetY;
+        changeMade = true;
+    }
 });
 
-canvas.addEventListener("mouseup", () => {
-  mode = null;
+canvas.addEventListener('mouseup', () => {
+    mode = null;
 });
+
 
 function isResizeMode(x, y) {
-  let leftSide = cropData.x - cropData.radius,
-    rightSide = cropData.x + cropData.radius;
-  return (
-    cropData.y - CONTROL_SIZE <= y &&
-    y <= cropData.y + CONTROL_SIZE &&
-    ((leftSide - CONTROL_SIZE <= x && x <= leftSide + CONTROL_SIZE) ||
-      (rightSide - CONTROL_SIZE <= x && x <= rightSide + CONTROL_SIZE))
-  );
+    let leftSide = cropData.x - cropData.radius,
+        rightSide = cropData.x + cropData.radius;
+    return cropData.y - SIZE_CONTROL <= y &&
+        y <= cropData.y + SIZE_CONTROL &&
+        (leftSide - SIZE_CONTROL <= x &&
+            x <= leftSide + SIZE_CONTROL ||
+            rightSide - SIZE_CONTROL <= x &&
+            x <= rightSide + SIZE_CONTROL)
 }
 
 function loadImage(file) {
-  return new Promise(resolve => {
-    let image = new Image();
-    image.addEventListener("load", function listener() {
-      image.removeEventListener("load", listener);
-      URL.revokeObjectURL(image.src);
-      resolve(image);
-    });
-    image.src = URL.createObjectURL(file);
-  });
+    return new Promise((resolve) => {
+        let image = new Image();
+        image.addEventListener('load', function listener() {
+            image.removeEventListener('load', listener);
+            URL.revokeObjectURL(image.src);
+            resolve(image);
+        });
+        image.src = URL.createObjectURL(file)
+    })
 }
 
 function render(image) {
-  let width = image.width,
-    height = image.height,
-    ctx = canvas.getContext("2d"),
-    cropCtx = cropCanvas.getContext("2d");
+    let width = image.width,
+        height = image.height,
+        ctx = canvas.getContext('2d'),
+        cropCtx = cropCanvas.getContext('2d');
 
-  frameRequest = requestAnimationFrame(() => {
-    if (changeMade) {
-      ctx.clearRect(0, 0, width, height);
-      ctx.drawImage(image, 0, 0, width, height);
-      cropCtx.save();
-      cropCtx.clearRect(0, 0, width, height);
-      cropCtx.fillStyle = "rgba(0,0,0,0.5)";
-      cropCtx.fillRect(0, 0, width, height);
-      cropCtx.globalCompositeOperation = "destination-out";
-      cropCtx.fillStyle = "#fff";
-      cropCtx.beginPath();
+    frameRequest = requestAnimationFrame(function () {
+        if (changeMade) {
+            ctx.clearRect(0, 0, width, height);
+            ctx.drawImage(image, 0, 0, width, height);
+            cropCtx.save();
+            cropCtx.clearRect(0, 0, width, height);
+            cropCtx.fillStyle = 'rgba(0,0,0,0.5)';
+            cropCtx.fillRect(0, 0, width, height);
+            cropCtx.globalCompositeOperation = 'destination-out';
+            cropCtx.fillStyle = '#fff';
+            cropCtx.beginPath();
+            cropCtx.arc(cropData.x, cropData.y, cropData.radius, 0, 2 * Math.PI);
+            cropCtx.closePath();
+            cropCtx.fill();
+            cropCtx.restore();
 
-      cropCtx.arc(cropData.x, cropData.y, cropData.radius, 0, 2 * Math.PI);
+            ctx.drawImage(cropCanvas, 0, 0, width, height);
 
-      cropCtx.closePath();
-      cropCtx.fill();
-      cropCtx.restore();
-
-      ctx.drawImage(cropCanvas, 0, 0, width, height);
-
-      changeMade = false;
-    }
-    render(image);
-  });
+            changeMade = false;
+        }
+        render(image);
+    });
 }
